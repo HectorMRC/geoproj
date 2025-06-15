00af2e3c0d11621bfb12ec08df06fdca1db0c84a
hectormrc <thehector1593@gmail.com>
2025-06-15 12:38:22 +0200

    feat: define projection trait

---
+++ README.md
@@ -0,0 +1,6 @@
+# geoproj
+
+[![Continuos Integration](https://github.com/hectormrc/geoproj/actions/workflows/ci.yml/badge.svg?branch=main)](https://github.com/hectormrc/geoproj/actions/workflows/ci.yml)
+[![Crates.io: geoproj](https://img.shields.io/crates/v/geoproj.svg)](https://crates.io/crates/geoproj)
+
+Projects geographic coordinates onto the plane. 
---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use boolygon::point::Point;
+use geocart::geographic::Geographic;
+
+/// A projection is a function that maps [`Geographic`] coordinates to [`Point`]s on the plane.
+pub trait Projection<T> {
+    /// The type of error that may occur when performing the projection.
+    type Error;
+
+    /// Projects the given [`Geographic`] coordinates onto the plane, returning the corresponding
+    /// [`Point`].
+    fn project(&self, coordinates: &Geographic<T>) -> Result<Point<T>, Self::Error>;
+
+    /// Performs the inverse of the projection, returning the [`Geographic`] coordiantes of the
+    /// given [`Point`].
+    fn inverse(&self, coordinates: &Point<T>) -> Result<Geographic<T>, Self::Error>;
+}
